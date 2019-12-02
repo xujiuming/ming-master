93923dcddf9b18346e80e6d0bd4ac21bf2e3d441
ming <18120580001@163.com>
2019-12-02 09:31:12 +0800

    feat (timeline): 将时间线抽为组件
    update timeline改为公用组件

--- docs/.vuepress/components/timeline.vue
+++ docs/.vuepress/components/timeline.vue
@@ -1,25 +1,56 @@
+<!--
+基于 vue-cute-timeline 自定义组件
+传入 eventList 构建时间线
+后续有需求了  添加多种属性  构建漂亮的
+-->
 <template>
     <div class="demo-them light">
         <timeline timeline-theme="lightblue">
             <timeline-title>未来</timeline-title>
-            <timeline-item bg-color="#9dd8e0" font-color="#000000">2019-12-01 19:00:00 github actions自动编译部署到oss</timeline-item>
-            <timeline-item bg-color="#9dd8e0" font-color="#000000">2019-12-01 14:00:00 主站改版(使用vuepress+github actions + oss)</timeline-item>
+            <timeline-item v-for="item in  eventList">{{item}}</timeline-item>
         </timeline>
     </div>
 </template>
 
 <script>
-    import { Timeline, TimelineItem, TimelineTitle } from 'vue-cute-timeline'
+    import {Timeline, TimelineItem, TimelineTitle} from 'vue-cute-timeline'
 
     export default {
         components: {
             Timeline,
             TimelineItem,
             TimelineTitle
+        },
+        props: {
+            eventList: Array
         }
     }
 </script>
 
 <style>
-    .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
+    .github-corner:hover .octo-arm {
+        animation: octocat-wave 560ms ease-in-out
+    }
+
+    @keyframes octocat-wave {
+        0%, 100% {
+            transform: rotate(0)
+        }
+        20%, 60% {
+            transform: rotate(-25deg)
+        }
+        40%, 80% {
+            transform: rotate(10deg)
+        }
+    }
+
+    @media (max-width: 500px) {
+        .github-corner:hover .octo-arm {
+            animation: none
+        }
+
+        .github-corner .octo-arm {
+            animation: octocat-wave 560ms ease-in-out
+        }
+    }
 </style>
--- docs/timeline/README.md
+++ docs/timeline/README.md
@@ -4,4 +4,17 @@ sidebar: false
 
 
 ## 时间线
-<timeline></timeline>
+<timeline :eventList="eventList"></timeline>
+
+<script >
+export default {
+      data(){
+        return {
+          eventList: [
+          '2019-12-01 19:00:00 github actions自动编译部署到oss',
+          '2019-12-01 14:00:00 主站改版(使用vuepress+github actions + oss)'
+          ]
+        };
+      }
+    }
+</script>
